1a25b6021590bc43c0a4a6457cba7c32238c4e49
alexkychen <alexkychen@gmail.com>
2017-06-06 23:52:09 -0400

    update tests

---
+++ tests/testthat/test_accuracy.R
@@ -0,0 +1,22 @@
+context("Test accuracy.xxx")
+
+test_that("Calculate assignment accuracy for Monte-Carlo results",{
+  AccuMC <- accuracy.MC(dir="ResMCtest/")
+  expect_output(str(AccuMC),"data.frame")
+  expect_true(file.exists("ResMCtest/Rate_of_3_tests_3_pops.txt"))
+  plot <- accuracy.plot(AccuMC)
+  expect_output(str(plot), "List of 9")
+})
+
+unlink("ResMCtest/Rate_of_3_tests_3_pops.txt")
+
+test_that("Calculate assignment accuracy for K-fold results",{
+  AccuKF <- accuracy.kfold(dir="ResKFtest/")
+  expect_output(str(AccuKF),"data.frame")
+  expect_true(file.exists("ResKFtest/Rate_of_3_tests_3_pops.txt"))
+  plot <- accuracy.plot(AccuKF)
+  expect_output(str(plot), "List of 9")
+})
+
+unlink("ResKFtest/Rate_of_3_tests_3_pops.txt")
+
